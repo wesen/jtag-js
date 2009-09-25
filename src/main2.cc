b925663d2eeb68a8e1f57438fd242efa747b19c4
Manuel Odendahl <wesen@ruinwesen.com>
2009-09-25 12:02:54 +0200

    added main2 cc

---
+++ src/main2.cc
@@ -0,0 +1,41 @@
+#include "avarice.h"
+#include "jtag.h"
+#include "jtag2.h"
+
+int main(int argc, char *argv) {
+	unsigned long jtagBitrate = 0;
+	const char *jtagDeviceName = NULL;
+	char *deviceName = 0;
+
+	bool isXmega = false;
+	bool isDragon = false;
+	bool applyNsrst = false;
+	const char *eventList = "none,run,target_power_on,target_sleep,target_wakeup";
+	
+	enum {
+		MKI, MKII, MKII_DW
+	} protocol = MKII;
+
+	jtagBitrate = 250000;
+	jtagDeviceName = "usb";
+	deviceName = "at90usb1287";
+
+	try {
+		jtag *theJtagICE = new jtag2(jtagDeviceName, deviceName, false, isDragon, applyNsrst, isXmega);
+		theJtagICE->dchain.units_before = 0;
+		theJtagICE->dchain.units_after = 0;
+		theJtagICE->dchain.bits_before = 0;
+		theJtagICE->dchain.bits_after = 0;
+		theJtagICE->parseEvents(eventList);
+		
+		theJtagICE->initJtagBox();
+	} catch (const char *msg) {
+		fprintf(stderr, "%s\n", msg);
+		return 1;
+	} catch (...) {
+		fprintf(stderr, "Cannot initialize JTAG ICE\n");
+		return 1;
+	}
+
+	theJtagICE->jtagReadFuses();
+}
