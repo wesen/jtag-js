7a4f7378d6ff456aee6dff9942d5ebae1b739e56
U-SUB\mnl <mnl@sub.(none)>
2009-09-29 20:25:39 +0200

    updated javascript files

--- js/jtag.js
+++ js/jtag.js
@@ -1,14 +1,68 @@
+load("helpers.js");
+
+// avr specific functions
+
 avr = {
-    RAM_ADDR: 0x800000
+    FLASH_SPACE_OFFSET:      0x000000,
+    RAM_SPACE_OFFSET:        0x800000,
+    EEPROM_SPACE_OFFSET:     0x810000,
+    FUSE_SPACE_OFFSET:       0x820000,
+    LOCK_SPACE_OFFSET:       0x830000,
+    SIG_SPACE_OFFSET:        0x840000,
+    BREAKPOINT_SPACE_OFFSET: 0x900000,
+
+    LOW_FUSE      : 0,
+    HIGH_FUSE     : 1,
+    EXTENDED_FUSE : 2,
+
+    BREAKPOINT_NONE : 0,
+    BREAKPOINT_CODE : 1,
+    BREAKPOINT_SOFTCODE : 2,
+    BREAKPOINT_WRITE_DATA : 3,
+    BREAKPOINT_READ_DATA : 4,
+    BREAKPOINT_ACCESS_DATA : 5
 };
 
-function ram(x) {
-    return x + avr.RAM_ADDR;
+avr.init = function () {
+    avr.ADDR_SPACE_MASK = 
+	avr.FLASH_SPACE_OFFSET |
+	avr.RAM_SPACE_OFFSET   |
+	avr.EEPROM_SPACE_OFFSET |
+	avr.FUSE_SPACE_OFFSET   |
+	avr.LOCK_SPACE_OFFSET   |
+	avr.SIG_SPACE_OFFSET    |
+	avr.BREAKPOINT_SPACE_OFFSET;
+};
+
+avr.ram = function (x) {
+    return x + avr.RAM_SPACE_OFFSET;
 }
 
-function samplePC() {
+avr.fuse = function (x) {
+    return x + avr.FUSE_SPACE_OFFSET;    
+}
+
+// jtag helper functions
+
+jtag.samplePC = function () {
     jtag.interrupt();
     var pc = jtag.getPC();
     jtag.resume();
     return pc;
+};
+
+jtag.writeFuse = function (fuse, fuseValue) {
+    return jtag.write(avr.fuse(fuse), [ fuseValue ]);
 }
+
+jtag.writeHighFuse = function (value) {
+    return jtag.writeFuse(avr.HIGH_FUSE, value);
+};
+
+jtag.writeLowFuse = function (value) {
+    return jtag.writeFuse(avr.LOW_FUSE, value);
+};
+
+jtag.writeExtendedFuse = function (value) {
+    return jtag.writeFuse(avr.EXTENDED_FUSE, value);
+};
